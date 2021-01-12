4479a4b8f6ce2daf78a0cc18eac4f1250d2991aa
Hao <hao09@yahoo.com>
2021-01-11 16:52:25 -0800

    improved css

--- Chat app/public/js/chatapp.js
+++ Chat app/public/js/chatapp.js
@@ -1,32 +1,31 @@
 var socket = io();
 var messages = document.getElementById("messages");
 
+
+//sending message to html
 (function() {
   $("form").submit(function(e) {
     let li = document.createElement("li");
-    e.preventDefault(); 
+    e.preventDefault(); //prevents message from dissappering
     socket.emit("chat message", $("#message").val());
     messages.appendChild(li).append($("#message").val());
     let span = document.createElement("span");
-    messages.appendChild(span).append("message by" + "user2");
-
-    $("#message").val("");
+    messages.appendChild(span).append("message by " + "user2");
 
     return false;
   });
-
+//handling the receiving side of the chat
   socket.on("received", data => {
     let li = document.createElement("li");
     let span = document.createElement("span");
     var messages = document.getElementById("messages");
     messages.appendChild(li).append(data.message);
-    messages.appendChild(span).append("message by" + "user1");
+    messages.appendChild(span).append("message by " + "user1");
   });
 })();
 
 let messageInput = document.getElementById("message");
 let typing = document.getElementById("typing");
-
 
 messageInput.addEventListener("keypress", () => {
   socket.emit("typing", { user: "Someone", message: "is typing..." });
