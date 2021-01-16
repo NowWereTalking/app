0b84bc867d1f6f8e2a86c219accf4af35a991f4c
Hao <hao09@yahoo.com>
2021-01-16 09:34:37 -0800

    added video with chat app

---
+++ server/routes/debateRoute.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const { v4: uuidV4 } = require('uuid');
+
+app.use(bodyParser.json());
+
+const Chat = require("./models/Chat");
+const connect = require("./dbconnect");
+
+io.on("connection", socket => {
+  console.log("user connected");
+
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+
+   socket.on("typing", data => {
+    socket.broadcast.emit("notifyTyping", {
+      user: data.user,
+      message: data.message
+    });
+  });
+  
+  socket.on("stopTyping", () => {
+    socket.broadcast.emit("notifyStopTyping");
+  });
+
+  socket.on("chat message", function(msg) {
+    console.log("message: " + msg);
+
+   
+    socket.broadcast.emit("received", { message: msg });
+
+        connect.then(db => {
+      console.log("connected correctly to the server");
+      let chatMessage = new Chat({ message: msg, sender: "user" });
+
+      chatMessage.save();
+    });
+  });
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+});
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ let express = require('express')
 let app = express() 
 let PORT = process.env.PORT || 3001
 
+var debateRoute = require('./routes/debateRoute');
+
 app.get('/test', (req, res) => {
     console.log("REQUEST WAS PROXIED")
     res.json({ done : true })
 })
+
+app.use('/debateRoute', debateRoute.js)
+
 
 app.listen(PORT, (err) => {
     if(err) console.log(err)
