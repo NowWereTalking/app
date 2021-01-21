66c4dccbbced8b4dd1e6ba2a4dbe4da16e9bb2d5
Irene Novelo <iyn88@yahoo.com>
2021-01-20 21:13:04 -0800

    combining code

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 function NavBar() {
-    return (
-      <div className="nav-container">
-          NavBar Goes HEre
-      </div>
-    );
-  }
+  return (
+      <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Now We're Talking</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                  <Link className="nav-item nav-link" to="/">Profile</Link>
+                  <Link className="nav-item nav-link" to="/users">Challengers</Link>
+                  <Link className="nav-item nav-link" to="/debate">Debate</Link>
+                  
+
+              </Nav>
+
+          </Navbar.Collapse>
+      </Navbar>
+  )
+}
   
   export default NavBar;
   
